43b7d7d69a1e03a7a234d9e0c27f9cccb4ddd096
ch106 <2957893818@qq.com>
2018-08-17 21:28:49 +0800

    add snsapi_base

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
+<?php
+require("./weixin_oop.php");
+$redirect_url = "http://chua.applinzi.com/logo.php";
+$appid = "wx5f7e76a499956419";
+$appsecret = "22ef2d429456869644a5ad2f6f7addcd";
+$wx = new weixinapi($appid, $appsecret);
+$base = $wx->snsapi_base($redirect_url);
+$openid = $base['openid'];
+echo $openid;
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
-    <meta http-equiv="refresh" content="2;url=exercise.html" />
+    <meta http-equiv="refresh" content="2;url=exercise.html"/>
     <title>百特美健身</title>
     <link rel="stylesheet" href="css/rem.css">
     <style type="text/css">
-        body{
+        body {
             background: #202020;
         }
+
         img {
             width: 100%;
             height: 100%;
